78e1661cd1d4ca389b3d910ead13ac7b6df698a8
francescoFH <francescofacca@gmail.com>
2021-08-18 00:17:16 +0100

    Added destory method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -14,7 +15,6 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-
     if @post.save
       flash[:success] = "The post was created!"
       redirect_to @post
@@ -29,12 +29,16 @@ class PostsController < ApplicationController
   def update
     if @post.update(post_params)
       flas[:success] = "Update successful!"
+      redirect_to @post
     else
       render 'edit'
     end
   end
 
   def destroy
+    @post.destory
+    flash[:success] = "Post destroyed!"
+    redirect_to root_path
   end
 
 private
