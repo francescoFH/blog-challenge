1bc1ddba77df2636b71d8e83c07f81f99b69faf3
francescoFH <francescofacca@gmail.com>
2021-08-17 23:51:53 +0100

    Added methods I will need for the Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+
+  def update
+  end
+
+  def destroy
+  end
+
+private
+
+  def post_params
+  end
+
+  def find_post
+  end
 end
